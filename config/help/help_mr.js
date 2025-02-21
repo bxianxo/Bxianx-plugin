771d6c2f94c51faf8118f2d5938071c3a7f2a70b
bxianxo <bxianxo@qq.com>
2025-02-21 15:51:56 +0800

    提交基本目录进行修改插件

---
+++ config/help/help_mr.js
@@ -0,0 +1,286 @@
+/*
+ * 此配置文件为系统使用，请勿修改，否则可能无法正常使用
+ *
+ * 如需自定义配置请复制修改上一级help_mb.js
+ *
+ **/
+
+// 主帮助配置
+export const helpCfg = {
+    title: "不羡仙帮助",
+    subTitle: "Yunzai - Bot & Bxianx - Plugin",
+    columnCount: 3,
+    colWidth: 265,
+    theme: "all",
+    themeExclude: ["mb"],
+    style: {
+      fontColor: "#ceb78b",
+      descColor: "#eee",
+      contBgColor: "rgba(6, 21, 31, .5)",
+      contBgBlur: 3,
+      headerBgColor: "rgba(6, 21, 31, .4)",
+      rowBgColor1: "rgba(6, 21, 31, .2)",
+      rowBgColor2: "rgba(6, 21, 31, .35)",
+    },
+    bgBlur: false,
+  };
+  
+  // 主帮助列表
+  export const helpList = [
+    {
+      group: "不羡仙查询帮助",
+      list: [
+        {
+          icon: 31,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 32,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 17,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 18,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 38,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 39,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 45,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 46,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 48,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 39,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 91,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 92,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 132,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 164,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 171,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 207,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 167,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 168,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 185,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 172,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 133,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 2,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 3,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 25,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 25,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 211,
+          title: "待定",
+          desc: "设置帮助",
+        },
+        {
+          icon: 66,
+          title: "待定设置帮助",
+          desc: "设置帮助",
+        },
+        {
+          icon: 67,
+          title: "待定插件更新",
+          desc: "更新插件",
+        },
+        {
+          icon: 26,
+          title: "待定",
+          desc: "待定",
+        },
+      ],
+    },
+  ];
+  
+  // 设置帮助配置
+  export const bqbhelpCfg = {
+    title: "不羡仙设置帮助",
+    subTitle: "Yunzai - Bot & Bxianx - Plugin",
+    columnCount: 3,
+    colWidth: 265,
+    theme: "all",
+    themeExclude: ["mr"],
+    style: {
+      fontColor: "#ceb78b",
+      descColor: "#eee",
+      contBgColor: "rgba(6, 21, 31, .5)",
+      contBgBlur: 3,
+      headerBgColor: "rgba(6, 21, 31, .4)",
+      rowBgColor1: "rgba(6, 21, 31, .2)",
+      rowBgColor2: "rgba(6, 21, 31, .35)",
+    },
+    bgBlur: false,
+  };
+  
+  // 设置帮助列表
+  export const bqbhelpList = [
+    {
+      group: "不羡仙设置",
+      list: [
+        {
+          icon: 8,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 79,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 42,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 19,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 318,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 345,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 294,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 131,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 101,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 56,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 296,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 90,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 324,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 321,
+          title: "待定",
+          desc: "待定",
+        },
+        {
+          icon: 312,
+          title: "待定",
+          desc: "待定",
+        },
+      ],
+    },
+  ];
+  // 系统标识
+  export const isSys = true;
